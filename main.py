3b2ccae97438a6734921085e432f6744894dd764
NarminD <66163480+NarminD@users.noreply.github.com>
2023-01-07 07:20:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+#defining a function 
+print("Introducing a sahabi:")
+def my_details(name, age, country):
+  print("My name is " + name + "," + " I'm " + age + " years old, and" + " my origin is from " + country + ".")
+
+my_details("Bilal ibn Rabah", "60", "Abbysinia")
+
+
+
+print ("Now let's calculate percentages: ")
+#returning a value 
+def percentage (x):
+  return (x/100) * 100
+
+print(percentage(25))
+print(percentage(50))
+print(percentage(78))
+
+
+#learning python functions 
